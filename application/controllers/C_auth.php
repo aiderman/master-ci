0c6a95326e049be2840b2b2bd5b4dca35efc62a6
Aider_m <aiderman347@gmail.com>
2024-01-17 22:12:46 +0800

    fix navbar

--- application/controllers/C_auth.php
+++ application/controllers/C_auth.php
@@ -8,7 +8,7 @@ class C_auth extends CI_Controller
         parent::__construct();
         $this->load->helper(array('form'));
         $this->load->helper('url');
-        $this->load->Model('M_pengguna');
+        $this->load->Model('t_user');
     }
 
     public function index()
@@ -16,34 +16,7 @@ class C_auth extends CI_Controller
         $user = $this->input->post('username');
         $pass = $this->input->post('password');
 
-        $data = $this->M_pengguna->userCek($user, $pass);
-
-
-        if ($data == TRUE) {
-
-            $session_data = array(
-                's_id'         => $data[0]->id_pengguna,
-                's_username'         => $data[0]->username,
-                's_password'         => $data[0]->password,
-                's_foto'            => $data[0]->foto,
-                's_level'            => $data[0]->level,
-            );
-            // echo "<pre>";
-            // echo print_r($session_data);
-            // echo "<pre>";
-            // die();
-            $this->session->set_userdata($session_data);
-            redirect('C_dash/');
-        } else {
-?>
-            <script type='text/javascript'>
-                alert('Nama Akun & Kata Sandi Anda Salah!');
-                history.back('C_login');
-            </script>
-<?php
-            $this->session->set_flashdata('fail2', "Gagal");
-            redirect('C_login');
-        }
+        $data = $this->t_user->userCek($user, $pass);
     }
 
 
